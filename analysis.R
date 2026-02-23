b70f4f3005456ecedef630b35858978ef38b48e1
amina <amina.ktari@insat.ucar.tn>
2026-02-23 15:24:49 +0100

    first commit

---
+++ analysis.R
@@ -0,0 +1,4 @@
+air_rearrangement <- read.delim(file = "VDJ/airr_rearrangement.tsv")
+all_contig <- read.delim("VDJ/all_contig_annotations.csv", sep = ",")
+concensus <- read.csv("VDJ/consensus_annotations.csv")
+
